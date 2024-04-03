6c22d34ed41285ddc32fbf988f57b72c8f8538ef
deriech <deriech73@gmail.com>
2024-04-03 17:29:14 -0400

    initial commit

---
+++ pyboy_test.py
@@ -0,0 +1,70 @@
+from pyboy import PyBoy
+from tkinter import Tk, Canvas, PhotoImage, mainloop
+import numpy as np
+from PIL import Image, ImageTk
+import threading
+
+
+isfree = True
+pics = []
+def writepics(data):
+    global isfree
+    if isfree:
+        isfree = False
+        global pics
+        pics = data
+        isfree = True
+
+def readpics():
+    if isfree :
+        return pics  
+    else:
+        return None
+        
+        
+def pyboy_stuff():
+    pyboy = PyBoy("galaga.gb")
+    while pyboy.tick():
+        writepics(pyboy.game_area()) 
+        pass
+    pyboy.stop()
+
+
+def tkinter_stuff():
+    mult = 8
+    WIDTH, HEIGHT = 32 * mult, 32 * mult
+    window = Tk()
+    canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
+    canvas.pack()
+    img = PhotoImage(width=WIDTH, height=HEIGHT)
+    
+    
+    def update(img):
+        try:
+            game_area = np.array(readpics())
+            game_area = np.repeat(game_area,mult,axis=0)
+            game_area = np.repeat(game_area,mult,axis=1)
+            np.shape(game_area)
+            img = ImageTk.PhotoImage(image = Image.fromarray(game_area))
+            canvas.create_image(WIDTH/2, HEIGHT/2, image=img)
+            
+         
+        except Exception as e:
+            print("The error is: ", e)
+            pass
+        canvas.after(5, lambda : update(img))
+    update(img)
+ 
+
+    mainloop()
+    
+t1 = threading.Thread(target=pyboy_stuff)
+t2 = threading.Thread(target=tkinter_stuff)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print("Done!")
